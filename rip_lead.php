5873077520623aa2d3679bc65e93bdc1946399ee
wowa24 <wowa24@bk.ru>
2024-07-30 13:47:17 +0300

    Update rip_lead.php

--- rip_lead.php
+++ rip_lead.php
@@ -1,11 +1,4 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    die('405 Method not allowed');
-}
-$clientToken    = 'dba5f410b6d118e29d57400b95fd5710e93ba3e8ac2fcde2d1db661b732d98fe3cac19df3d1b5156'; // Токен со страницы Настройки, обязательно
-$phone          = trim(strip_tags($_POST['phone'])); // Телефон, обязательно
-$message        = trim(strip_tags(isset($_POST['message']) ? $_POST['message'] : '')); // Комментарий, не обязательно
-$redirectUrl = '/';
 $curl = curl_init();
 curl_setopt_array($curl, [
     CURLOPT_URL            => "https://cpa.ruki-iz-plech.ru/api/leads/create",
@@ -15,22 +8,22 @@ curl_setopt_array($curl, [
     CURLOPT_TIMEOUT        => 30,
     CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST  => "POST",
-    CURLOPT_SSL_VERIFYHOST => false,
-    CURLOPT_SSL_VERIFYPEER => false,
     CURLOPT_POSTFIELDS     => json_encode([
-        "phone"   => $phone,
-        "message" => $message,
-        "source"  => 'WordPress',
-    ], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES),
+        "phone"   => "74991106316", // номер клиента, обязательно
+        "message" => "Комментарий", // комментарий, не обязательно
+    ], JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES),
     CURLOPT_HTTPHEADER     => [
-        "auth: {$clientToken}",
+        "auth: dba5f410b6d118e29d57400b95fd5710e93ba3e8ac2fcde2d1db661b732d98fe3cac19df3d1b5156", // токен со страницы Настройки, обязательно
         "content-type: application/json",
     ],
 ]);
 $response = curl_exec($curl);
-$err      = curl_error($curl);
+$err = curl_error($curl);
 curl_close($curl);
 if ($err) {
-    die("cURL Error #:" . $err);
+    echo "cURL Error #:" . $err;
+} else {
+    echo $response;
 }
-header("Location: {$redirectUrl}");
+
+
